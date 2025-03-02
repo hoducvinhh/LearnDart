de07188123bc418930fdc50d35b65312305f9bcd
hoducvinhh <hoducvinh2k4@gmail.com>
2025-03-02 08:28:50 +0700

    done

---
+++ Typedef.dart
@@ -0,0 +1,6 @@
+// typedef trong dart la mot cach ngan gon de tao ra cac alias(bi danh) cho cac loai du lieu, dieu nay giup ma nguon tro nen ro rang hon
+
+void main() {
+  List<int> l1 = [1, 2, 3, 4];
+  print(11);
+}
