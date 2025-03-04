95082b237bdacfbbe023ae83ab56b66ba5f67503
hoducvinhh <hoducvinh2k4@gmail.com>
2025-03-04 07:30:04 +0700

    done

---
+++ caulenhdieukien.dart
@@ -0,0 +1,12 @@
+void main() {
+  // bool isNumBer = false;
+  // bool lasoChan = true;
+
+  // if (isNumBer) {
+  //   print("Day la mot conso");
+  // } else if (lasoChan) {
+  //   print("Day la so chan");
+  // } else {
+  //   print("-----");
+  // }
+}
